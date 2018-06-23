0a640194a087882bcb0bcacd2389bd7008c93136
Buhori Dermawan <buhori.dermawan@bizzy.co.id>
2018-06-23 21:03:59 +0700

    Setup unit test command

--- config/Route.js
+++ config/Route.js
@@ -1,21 +1,17 @@
 
 import Auth from '../controllers/AuthController';
-import UserController from '../controllers/UserController';
-import TaskController from '../controllers/TaskController';
+import User from '../controllers/UserController';
+import Task from '../controllers/TaskController';
 
 module.exports = function (app) {
-    // const auth = new AuthController(app);
-    const user = new UserController(app);
-    const task = new TaskController(app);
-
     app.route('/auth/login').post(Auth.login);
     app.route('/auth/register').post(Auth.register);
 
-    app.route('/user/list').get(user.loginRequired).get(user.list);
+    app.route('/user/list').get(User.loginRequired).get(User.list);
 
-    app.route('/task/create').post(user.loginRequired).post(task.create);
-    app.route('/task/list').get(user.loginRequired).get(task.list);
-    app.route('/task/view/:id').get(user.loginRequired).get(task.view);
-    app.route('/task/update/:id').post(user.loginRequired).post(task.update);
-    app.route('/task/delete/:id').post(user.loginRequired).post(task.delete);
+    app.route('/task/create').post(User.loginRequired).post(Task.create);
+    app.route('/task/list').get(User.loginRequired).get(Task.list);
+    app.route('/task/view/:id').get(User.loginRequired).get(Task.view);
+    app.route('/task/update/:id').post(User.loginRequired).post(Task.update);
+    app.route('/task/delete/:id').post(User.loginRequired).post(Task.delete);
 };
