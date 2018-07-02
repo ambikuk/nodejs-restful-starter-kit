01ce324cec1b9d24e2a065d9d8e49dc1a31224ea
insanrizky <insansan9@gmail.com>
2018-07-02 13:02:29 +0700

    Route

--- config/Route.js
+++ config/Route.js
@@ -2,18 +2,6 @@
 import Auth from '../controllers/AuthController';
 import Task from '../controllers/TaskController';
 import AuthMiddleware from '../middlewares/AuthMiddleware';
-// import Response from '../components/Response';
-// import TaskRepo from '../repositories/task';
-
-const express = require('express');
-
-const sample = express.Router();
-
-sample.get('/one', (req, res) => {
-    console.log(req);
-    res.json({ example: 'one' });
-});
-sample.route('/two').get(Task.view);
 
 module.exports = function (app) {
     app.route('/auth/login').post(Auth.login);
@@ -23,5 +11,4 @@ module.exports = function (app) {
     app.route('/task/:id').get(Task.view);
     app.route('/task/:id').put(AuthMiddleware.loginRequired).put(Task.update);
     app.route('/task/:id').delete(AuthMiddleware.loginRequired).delete(Task.delete);
-    app.use('/sample', sample);
 };
